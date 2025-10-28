dab696aa6e5301845f8187393d396b02d59da445
Karan Satyawali <kar.ansatyawali349@gmail.com>
2025-10-28 19:24:20 +0530

    Create 05_Dictionary.py
    Python Dictionary

---
+++ 01_Programming/Python/05_Dictionary.py
@@ -0,0 +1,27 @@
+#Dictionary
+
+marks = {
+    "Karan": 90,
+    "Rohan": 50,
+    "Sahil": 23
+}
+
+#print(marks["Karan"])
+#print(marks, type(marks))
+
+#dictionary methods
+
+marks = {
+    "Karan": 90,
+    "Rohan": 50,
+    "Sahil": 23
+}
+
+len(marks) # gives the number of key pairs present in dictionary
+#print(marks.items())
+#print(marks.keys())
+#print(marks.values())
+#marks.update({"Karan": 99})
+#print(marks)
+# print(marks.get("Karan2")) # prints None
+# print(marks["Karan2"]) # will return an error bcz there is no key of name karan2
