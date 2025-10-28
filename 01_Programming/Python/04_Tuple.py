15246abb2bedf1603c002ce0cc01c418f601ce1d
Karan Satyawali <kar.ansatyawali349@gmail.com>
2025-10-28 19:23:11 +0530

    Create 04_Tuple.py
    Python Tuple

---
+++ 01_Programming/Python/04_Tuple.py
@@ -0,0 +1,16 @@
+# TUPLES IN PYTHON 
+# A tuple is an immutable data type in python
+
+a = ("Karan",1, 3, 7,False)
+print(a)
+print(type(a))
+
+#   Tuples methods
+
+a1 = (1,2,3,8,98, 5,8,98,4,7)
+print(a1)
+
+new = a1.count(98) # returns number of times 1 occurs in a
+print(new)
+new1 = a1.index(2) # will return the index of 2
+print(new1)
