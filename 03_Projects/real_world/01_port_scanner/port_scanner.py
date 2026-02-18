4fb71f23bfe9052e07f42726ebeecef1661a153c
Karan Satyawali <kar.ansatyawali349@gmail.com>
2026-02-18 20:08:49 +0530

    Update port_scanner.py

--- 03_Projects/real_world/01_port_scanner/port_scanner.py
+++ 03_Projects/real_world/01_port_scanner/port_scanner.py
@@ -50,7 +50,7 @@ print("Scanning...\n")
 try:
     for port in ports_to_scan:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(1)
+        sock.settimeout(0.3)
 
         result = sock.connect_ex((target_ip, port))
 
